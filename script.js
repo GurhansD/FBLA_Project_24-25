7220d4848ee2ebcf614a43c2e8482ff8b4c038aa
Gurhans Dhillon <hansd2248@gmail.com>
2024-09-02 16:30:27 -0400

    Created Homepage Base

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Javascript is working")
+var count = 0
+//This function will determine whether or not to mute the background audio by using a counter
+function muteButtonClick() { 
+    count += 1;
+    const audio = document.getElementById('rain-audio');
+    if(count % 2 == 1){
+        audio.muted = false;
+    } else{
+        audio.muted = true;
+    }
+}
+
+
+//This line of code will run the muteButtonClick function everytime the mute button is clicked
+muteButton.addEventListener("click", muteButtonClick);
