932435fd1ab2146b1b5e106bfffe31b6f5b73141
Gurhans Dhillon <hansd2248@gmail.com>
2025-02-14 15:45:20 -0500

    Story Development (subwayPathMaintenence) and Added more "Lose" Paths

--- textAnimation.js
+++ textAnimation.js
@@ -1,14 +1,24 @@
 export function animateText(elementId, text, speed, callback) {
     const element = document.getElementById(elementId);
     let index = 0;
+    let isAnimating = true;
+
+    // Disable buttons during animation
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => button.disabled = true);
 
     function type() {
         if (index < text.length) {
             element.innerHTML += text.charAt(index);
             index++;
             setTimeout(type, speed);
-        } else if (callback) {
-            callback(); // Animation complete, invoke callback
+        } else {
+            isAnimating = false;
+            // Re-enable buttons after animation
+            buttons.forEach(button => button.disabled = false);
+            if (callback) {
+                callback(); // Animation complete, invoke callback
+            }
         }
     }
 
